fd2a7760e2d6fe5dde2b9251c20049c3f02496c0
aigerimass <assylkhanova0@gmail.com>
2022-12-24 23:46:54 +0300

    views

--- views.sql
+++ views.sql
@@ -2,9 +2,20 @@ create schema view_internships;
 
 set search_path = view_internships, public;
 
-drop view if exists view_candidates_rating;
-create view view_candidates_rating as
+drop view if exists view_mentors;
+create view view_mentors as
     select first_name || ' ' || mentors.last_name as "Имя ментора",
            regexp_replace(mentors.email, '[[:alpha:]]', '*') as "Email",
            case when team_name isnull then 'Без команды' else team_name end as "Название команды"
-    from internships.mentors
+    from internships.mentors;
+
+drop view if exists view_projects;
+create view view_projects as
+    select
+        projects.id as "№",
+        project_name as "Название проекта",
+        design_doc_link as "Дизайн-док",
+        first_name || ' ' || last_name as "Имя ментора"
+    from internships.projects inner join mentors m on m.id = projects.mentor_id;
+
+
